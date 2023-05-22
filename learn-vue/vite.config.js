a829883f0ddd379a37f48723c269c45eec2ae134
Zhao Shicao <zhaoshcao@foxmail.com>
2023-05-22 17:00:03 +0800

    使用pnpm管理项目

--- learn-vue/vite.config.js
+++ learn-vue/vite.config.js
@@ -1,15 +1,7 @@
-import {fileURLToPath, URL} from 'node:url'
-
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        },
-        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
-    }
+  plugins: [vue()],
 })

9c1cc58ef459b31429ec0a20365fc16b84e2dd6a
Zhao Shicao <zhaoshcao@foxmail.com>
2023-05-23 18:05:11 +0800

    完成筛选功能

--- learn-vue-ts/src/components/Task/Action.vue
+++ learn-vue-ts/src/components/Task/Action.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-
+import { ref } from 'vue'
+const input = ref('')
 </script>
 
 <template>
     <header>
-
+        <el-input v-model="input" placeholder="Please input" @input="$emit('filterTasks',input)"/>
     </header>
 </template>
 
--- learn-vue-ts/src/components/Task/index.vue
+++ learn-vue-ts/src/components/Task/index.vue
@@ -1,49 +1,37 @@
 <script setup lang="ts">
 import TaskTable from './Table.vue'
 import Action from './Action.vue'
-import { reactive, ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { task } from './index.js';
 
-// export interface task {
-//     userId: number,
-//     id: number,
-//     title: string,
-//     completed: boolean
-// }
-const data: task[] = [
-    {
-        "userId": 1,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": true
-    },
-    {
-        "userId": 1,
-        "id": 2,
-        "title": "quis ut nam facilis et officia qui",
-        "completed": false
-    },
-    {
-        "userId": 1,
-        "id": 3,
-        "title": "fugiat veniam minus",
-        "completed": false
+const tasks = ref<task[]>([])
+
+onMounted(async () => {
+    try {
+        const data: task[] = await (await fetch('https://jsonplaceholder.typicode.com/todos')).json()
+        tasks.value = data
+    } catch (error) {
+        console.log(error);
     }
-]
 
-const tasks:task[] = reactive(data)
 
-function changeCompleted(task: task) {
-    // console.log(task);
-    // console.log(task.completed);
-    task.completed=task.completed
+})
+
+
+async function filterTasks(taskId: number) {
+    try {
+        const data: task = await (await fetch(`https://jsonplaceholder.typicode.com/todos/${taskId}`)).json()
+        tasks.value = [data]
+    } catch (error) {
+        console.log(error);
+    }
 }
 </script>
 
 <template>
     <div class="container">
-        <Action />
-        <TaskTable :tasks="tasks" @change-completed="changeCompleted" />
+        <Action @filter-tasks="filterTasks" />
+        <TaskTable :tasks="tasks" />
     </div>
 </template>
 
@@ -54,6 +42,5 @@ function changeCompleted(task: task) {
     margin: 50px auto;
     height: 800px;
     width: 1600px;
-    /* background-color: antiquewhite; */
 }
 </style>

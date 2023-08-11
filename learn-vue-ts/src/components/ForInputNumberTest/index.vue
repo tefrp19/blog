52e2b641a55a0753d916b2a2ae14760f8754f668
fanrunping <1633467021@qq.com>
2023-08-11 11:16:22 +0800

    learning: input type 为 number 的测试

---
+++ learn-vue-ts/src/components/ForInputNumberTest/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-input-number v-model="num" @change="handleChange" @blur="handleBlur"/>
+  <input type="number" @change="handleNativeChange" @blur="handleNativeBlur" @keypress="handleNativeKeydown">
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const num = ref()
+
+const handleChange = (value) => {
+  console.log("change",value)
+}
+
+const handleBlur = (e) => {
+  console.dir("blur", e)
+  // console.dir("blur", e.target)
+  console.log("num",num.value)
+}
+
+const handleNativeChange=(e)=>{
+
+  // console.log(e.target.value)
+}
+
+const handleNativeBlur=(e)=>{
+  console.dir(e.target)
+  console.dir(e.target.value)
+}
+
+const handleNativeKeydown=(e)=>{
+  // console.log("keyCode",e.keyCode)
+  // console.log("String",String.fromCharCode(e.keyCode))
+  // return (/[\d]/.test(String.fromCharCode(e.keyCode)))
+  // if (String.fromCharCode(e.keyCode)==="1") return
+}
+
+</script>
+
+<style scoped>
+
+</style>

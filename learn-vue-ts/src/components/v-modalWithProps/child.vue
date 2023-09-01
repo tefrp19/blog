d743dce5191807aa2d918b3215372050b2b64564
fanrunping <1633467021@qq.com>
2023-09-01 14:46:33 +0800

    learning: 使用 model-value 单向绑定 dialog visible 值

---
+++ learn-vue-ts/src/components/v-modalWithProps/child.vue
@@ -0,0 +1,26 @@
+<!--使用 model-value 单向绑定 dialog visible 值，不使用 v-model 进行双向绑定-->
+<template>
+  <el-dialog
+      :model-value="dialogVisible"
+      @close="$emit('closeDialog')"
+      title="Tips"
+  >
+  </el-dialog>
+  <el-button @click="fn(dialogVisible)">测试在template中解构props</el-button>
+</template>
+
+<script setup lang="ts">
+const props=defineProps<{
+  dialogVisible:boolean
+}>()
+
+const fn=(param)=>{
+  console.log(param)
+}
+</script>
+
+
+
+<style scoped>
+
+</style>

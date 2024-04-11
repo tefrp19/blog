b6727762a95ec83daacb92c6fd6c3a634e3aa8f6
fanrunping <1633467021@qq.com>
2024-04-11 15:09:48 +0800

    添加src的别名:@

--- learn-vue-ts/vite.config.ts
+++ learn-vue-ts/vite.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsxPlugin from "@vitejs/plugin-vue-jsx";
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-
+import path from 'path' // 关键代码
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),vueJsxPlugin(),nodePolyfills()],
   server:{
     port: 5000
+  },
+  resolve: {
+    alias: {
+      // 关键代码
+      '@': path.resolve(__dirname, './src')
+    }
   }
 })
